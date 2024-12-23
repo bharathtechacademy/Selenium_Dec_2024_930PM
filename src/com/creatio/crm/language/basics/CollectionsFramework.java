9b80e1f1f5034b48de1a8d461752372b6149edae
bharathautomation <soft.ittech20@gmail.com>
2024-12-23 23:21:38 +0530

    Classes and Objects and collections

---
+++ src/com/creatio/crm/language/basics/CollectionsFramework.java
@@ -0,0 +1,16 @@
+package com.creatio.crm.language.basics;
+
+public class CollectionsFramework {
+
+	public static void main(String[] args) {
+		
+		//1. No need to specify the size of values while creating/initializing collections
+		//2. It follows dynamic memory allocation
+		//3. Collections allow multiple data types together stored at one place
+		//4. Collections allows modifications
+		
+		//5. There are so many inbuilt functions/ utilities to manipulate the data /filter the data
+
+	}
+
+}
